5bc5bdafcc45703d58d6fcec2283e0593b4950c6
David <davbetm@gmail.com>
2019-04-18 18:03:09 -0500

    Estructura base y menú

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        <link rel="stylesheet" href="css/master.css">
+        <link rel="stylesheet" href="css/menu.css">
+    </head>
+    <body>
+        <!-- Para incluir el menú -->
+        <?php
+            include_once("menu.html");
+        ?>
+    </body>
+</html>
